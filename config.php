d158dc7889f12310f488fc334788901689917960
Malte Dudd <malte.dudd@gmail.com>
2023-07-31 21:47:40 +0200

    Add Example CONFIG.PHP

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+$feeds = array(
+    array("NAME OF FIRST FEED", "###URL OF FIRST FEED","####UPDATEINTERVAL FOR THE HEARTBEAT OF FEED IN SECONDS###"),
+    array("NAME OF 2nd FEED", "###URL OF 2nd FEED","####UPDATEINTERVAL FOR THE HEARTBEAT OF FEED IN SECONDS###")
+  );
+
+//Base URL without trailing slash at the end
+  $baseUrl="###BASE URL WITHOUT SLASH AT THE END###";
+
+//API-Token and Chat ID for Telegram   
+  $apiToken = "##API TOKEN###";
+  $chatId = "CHAT ID";
+
+
+?>
+
